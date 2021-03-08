e5eca5093a961aa1bd93541c23861be9ee9ccfb7
Damian Bertoni <damianberto@gmail.com>
2021-03-08 09:34:39 -0300

    feat: Add test_get_gene_suggest

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from app import app
+
+
+def test_get_gene_suggest():
+    # Create a test client using the Flask application configured for testing
+    with app.test_client() as test_client:
+        response = test_client.get('/gene_suggest?species=homo_sapiens&query=te&limit=3')
+        assert response.status_code == 200
+        assert response.json == {"gene_names": [{"0": "ASTE1"}, {"1": "ATE1"}, {"2": "ATE1-AS1"}]}
