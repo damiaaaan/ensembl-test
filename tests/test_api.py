375f56301afe785ecbe92b7c3fea07ad7ef2cb95
Damian Bertoni <damianberto@gmail.com>
2021-03-08 10:29:01 -0300

    feat: add test for validate when species does not exist

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,3 +7,8 @@ def test_get_gene_suggest():
         response = test_client.get('/gene_suggest?species=homo_sapiens&query=te&limit=3')
         assert response.status_code == 200
         assert response.json == {"gene_names": [{"0": "ASTE1"}, {"1": "ATE1"}, {"2": "ATE1-AS1"}]}
+
+        # Test wrong 'sepecies' parameter
+        response = test_client.get('/gene_suggest?species=homo_sapient&query=te&limit=3')
+        assert response.status_code == 400
+        assert response.json == {"description": "Species does not exist", "error": 400, "name": "Bad Request"}
